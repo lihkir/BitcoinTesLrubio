241e2d60fab910db32b85f6985819ae8c56a3e7f
lihkir <likyalpha@gmail.com>
2020-01-08 00:15:46 -0500

    Fixing normalization error

--- sige_reg_der_der.m
+++ sige_reg_der_der.m
@@ -1,5 +1,7 @@
 function v = sige_reg_der_der(phi)
-    
+
+global epsilon phic;
+
 v = exp_reg(phi)*(sige_der_der(phi) + 4*sige_der(phi)*quot_reg(phi) + 2*epsilon*sige(phi)*(2*epsilon - 3*(phi - phic)^2));
 
 end
--- test_imex.m
+++ test_imex.m
@@ -7,13 +7,13 @@ format short
 %%
 global rho_s g mu_f vinf nexp phimax phi0 wrt_freq beta int_form D0 idx_q phic sigma0 kexp sigedc sigeddc diff_idx epsilon mu_g;
 
-n = 400;
-T = 0.07;
+n = 200;
+T = 10000;
 cfl = 0.6;
 L = 1;
 convec_type = 0;
 imex_type = 1;
-d1 = 4.96e-4;
+d1 = 1.19e-5;
 test = 1;
 %%
 
@@ -23,13 +23,12 @@ D0 = 1e-7;
 idx_q = 9;
 int_form = 1;
 g = 9.81; 
-
-mu_f = 10e-3; 
+mu_f = 1e-3; 
 rho_s = 1800; 
-vinf = mu_f*rho_s;
-mu_g = 1/g*rho_s;
+vinf = g*d1*d1*rho_s/(18*mu_f);
+mu_g = vinf/rho_s;
 
-phi0 = ((nexp-2)*phimax-1)/(nexp-3);
+phi0 = ((nexp - 2)*phimax-1)/(nexp - 3);
 phimax = 0.66;
 nexp = 4.7;
 phic = 0.2;
@@ -38,7 +37,7 @@ kexp = 2;
 sigedc = sigma0*kexp/phic;
 sigeddc = sigma0*kexp*(kexp - 1)/phic^2;
 epsilon = 1e-5;
-diff_idx = 1;
+diff_idx = 2;
 
 %%
 u = rk122imex(n, T, cfl, L, convec_type, imex_type, test);
